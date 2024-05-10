8fd6d468c56c23ce092b4d2864f3e91531f05678
Alejandro Rodriguez <alu0101413938@ull.edu.es>
2024-05-10 15:54:26 +0000

    Transaction Model

---
+++ src/models/transactions.ts
@@ -0,0 +1,52 @@
+import { Schema, model, Document } from "mongoose";
+import { CustomerSchema } from "./customers.js";
+import { ProviderSchema } from "./providers.js";
+import { FurnitureSchema } from "./furnitures.js";
+
+export interface TransactionDocumentInterface extends Document {
+  type: string,
+  furniture: typeof FurnitureSchema[],
+  customer?: typeof CustomerSchema,
+  provider?: typeof ProviderSchema,
+  pay: number,
+  time: Date,
+}
+
+export const TransactionSchema = new Schema<TransactionDocumentInterface>({
+  type: {
+    type: String,
+    enum: ["Sell", "Buy"],
+    required: true,
+  },
+  furniture: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Furniture",
+      required: true,
+    },
+  ],
+  customer: {
+    type: Schema.Types.ObjectId,
+    ref: "Customer",
+  },
+  provider: {
+    type: Schema.Types.ObjectId,
+    ref: "Provider",
+  },
+  pay: {
+    type: Number,
+    required: true,
+    validate: (value :number) => {
+      if (value <0) {
+        throw new Error("Transaction pay must be positive number");
+      }
+    },
+  },
+  time: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+export const Customers = model<TransactionDocumentInterface>('Transaction', CustomerSchema);
