fa29892eed425e552c65e6d3842c92ead2e465f7
Alejandro Rodriguez <alu0101413938@ull.edu.es>
2024-05-10 20:31:19 +0000

    Refresh

--- src/App/transactions.ts
+++ src/App/transactions.ts
@@ -26,10 +26,26 @@ transactionRouter.post('/transaction', async (req :Request, res :Response) => {
     const {humanId, type} = req.body;
     const human = await Customers.findById(humanId) || await Providers.findById(humanId);
     if (!human) return res.status(404).send("Human not found");
+    const customer = await Customers.findById(humanId);
+    const provider = await Providers.findById(humanId);
+    const furnitures = await req.body.furniture;
+    if (furnitures.length <= 0) return res.status(404).send("Furnitures has been not provide");
     if (type == "Sell") { // Proveedor vende
-
+      if (customer) return res.status(404).send("You need to introduce a provider, not a customer")
+      const transaction = new Transactions(req.body);
+      transaction.save().then((tsn) => {
+        res.send(tsn);
+      }).catch((err) => {
+        res.status(400).send(err);
+      })
     } else if (type == "Buy") { // Customer compra
-      
+      if (provider) return res.status(404).send("You need to introduce a customer, not a provider")
+        const transaction = new Transactions(req.body);
+      transaction.save().then((tsn) => {
+        res.send(tsn);
+      }).catch((err) => {
+        res.status(400).send(err);
+      })
     }
   } catch (error) {
     res.status(400).send(error);
