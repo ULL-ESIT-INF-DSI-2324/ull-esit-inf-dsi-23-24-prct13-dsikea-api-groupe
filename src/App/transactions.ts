d1a079f5efe352a669c4d350b3eef8e9ab76f702
Alejandro Rodriguez <alu0101413938@ull.edu.es>
2024-05-11 18:04:25 +0000

    Refresh

--- src/App/transactions.ts
+++ src/App/transactions.ts
@@ -1,5 +1,8 @@
 import express, {Request, Response} from 'express'
 import { Transactions } from '../models/transactions.js'
+import { Providers } from '../models/providers.js';
+import { Customers } from '../models/customers.js';
+import { Furnitures } from '../models/furnitures.js';
 
 export const transactionRouter = express.Router();
 
@@ -8,7 +11,7 @@ transactionRouter.get('/transactions', async (req :Request, res :Response) => { 
     const {id, time, type, pay} = req.query;
     let transaction;
     if (id) {
-      transaction = await Transactions.findById({ id })
+      transaction = await Transactions.findOne({ _id: id })
     } else if (time) {
       transaction = await Transactions.findOne({ time: time })
     } else if (type) {
@@ -18,6 +21,50 @@ transactionRouter.get('/transactions', async (req :Request, res :Response) => { 
     } else {
       transaction = await Transactions.find()
     }
+    if (!transaction) return res.status(404).send("Does not exist any transaction.");
+    res.send(transaction);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
+transactionRouter.get('/transactions/:id', async (req :Request, res :Response) => {
+  try {
+    const id = req.params.id;
+    const transaction = await Transactions.findOne({ _id: id });
+    if (!transaction) return res.status(404).send("Transaction not found");
+    res.send(transaction);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
+transactionRouter.get('/transactions/:time', async (req :Request, res :Response) => {
+  try {
+    const time = req.params.time;
+    const transaction = await Transactions.findOne({ time: time });
+    if (!transaction) return res.status(404).send("Transaction not found");
+    res.send(transaction);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
+transactionRouter.get('/transactions/:pay', async (req :Request, res :Response) => {
+  try {
+    const pay = req.params.pay;
+    const transaction = await Transactions.findOne({ pay: pay });
+    if (!transaction) return res.status(404).send("Transaction not found");
+    res.send(transaction);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
+transactionRouter.get('/transactions/:type', async (req :Request, res :Response) => {
+  try {
+    const type = req.params.type;
+    const transaction = await Transactions.findOne({ type: type });
     if (!transaction) return res.status(404).send("Transaction not found");
     res.send(transaction);
   } catch (error) {
@@ -28,6 +75,31 @@ transactionRouter.get('/transactions', async (req :Request, res :Response) => { 
 transactionRouter.post('/transactions', async (req :Request, res :Response) => {
   try {
     const transaction = new Transactions(req.body);
+    const isProvider = new Providers(req.body.provider);
+    const isCustomer = new Customers(req.body.customer);
+    const furnitures = req.body.furniture;
+    let human;
+    if (furnitures.length <= 0) return res.status(404).send("You need to provide any furnitures")
+    switch (req.body.type) {
+      case "Buy":
+        if (isProvider) return res.status(404).send("You need to provide a consumer")
+        human = await Customers.findOne({ _id: isCustomer });
+        if (!human) return res.status(404).send("You need to provide a correct ID for the customer")
+        break;
+      case "Sell":
+        if (isCustomer) return res.status(404).send("You need to provide a provider")
+        human = await Providers.findOne({ _id: isProvider });
+        if (!human) return res.status(404).send("You need to provide a correct ID for the provider")
+        break;
+      default:
+        return res.status(404).send("An error has ocurred");
+        break;
+    }
+    for (let i = 0; i < furnitures.length; i++) {
+      const fns = furnitures[i];
+      const fn = await Furnitures.findOne(fns);
+      if (!fn) return res.status(404).send("You need to provide a correct furnitures IDs");
+    }
     transaction.save().then((transaction) => {
       res.send(transaction);
     }).catch((err) => {
@@ -35,5 +107,16 @@ transactionRouter.post('/transactions', async (req :Request, res :Response) => {
     })
   } catch (error) {
     res.status(404).send(error);
+  }
+})
+
+transactionRouter.delete('transactions/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const transaction = await Transactions.findByIdAndDelete({ id })
+    if (!transaction) return res.status(404).send("Transactions not found");
+    res.send("Transactions has been removed");
+  } catch (error) {
+    res.status(400).send(error);
   }
 })
