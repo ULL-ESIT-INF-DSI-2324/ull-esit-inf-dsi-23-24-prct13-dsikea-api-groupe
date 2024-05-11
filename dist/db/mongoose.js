33aacc75814c5ae66ba356c69ea32d3a26708222
Alejandro Rodriguez <alu0101413938@ull.edu.es>
2024-05-11 16:43:43 +0000

    Refresh

--- dist/db/mongoose.js
+++ dist/db/mongoose.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
-const url = process.env.MONGODB_URL || "mongodb+srv://dsikea-rest-api:dsikea@clusterdsikea.nhxvu4f.mongodb.net/dsikea";
-const connectDB = async () => {
-    await mongoose.connect(url).then(() => {
-        console.log('Connection to MongoDB server stablished.');
-        console.log(`The db is conencted with ${mongoose.connection.host}`);
-    }).catch(() => {
-        console.log('Unable to connect to MongoDB server.');
-    });
-};
-connectDB();
+try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('Connection to MongoDB server established');
+    console.log(`Host: ${mongoose.connection.host}`);
+}
+catch (error) {
+    console.log(error);
+}
